b67e15838b1ddce0d7df644929469fae4696c1d7
AdCalleja <adri.calleja.1999@gmail.com>
2024-05-27 23:41:24 +0200

    Change to crt0. Added bootservices of Limine

--- kernel_riscv/src/asm/crt0.s
+++ kernel_riscv/src/asm/crt0.s
@@ -125,119 +125,106 @@ halt_forever:
   wfi
   j halt_forever
 
-# # Interrupt handler
-# interrupt_handler:
-#   # Save all general purpose registers into kernel trap frame
-#   # No need to save floating point registers since we haven't used them yet
-#   # No need to save satp, trap_stack since we don't modify them
-#   # No need to save hartid since that is always 0
-#   # (we only have a single CPU core)
-#   # This requires a bit of trickery to do correctly:
-#   # 
-#   # 0. mscratch has address of kernel trap frame - see kinit() for details
-#   # 1. Atomically swap mscratch and t6 registers
-#   #    Now t6 has address of kernel trap frame, and mscratch the
-#   #    original value of t6
-#   # 2. Now save registers x1-x30 into kernel trap frame, using
-#   #    t6=x31 as base
-#   #    No need to save zero=x0 since that is read-only zero
-#   # 3. Move address of kernel trap frame to t5=x30 so we don't lose it
-#   # 4. Move mscratch (= original value of t6) back into t6 and save that
-#   # 5. Write address of kernel trap frame from t5 back into mscratch
-#   csrrw t6, mscratch, t6
-#   .set i, 1
-#   .rept 30
-#     save_gp %i
-#     .set i, i + 1
-#   .endr
-#   mv t5, t6
-#   csrr t6, mscratch
-#   save_gp 31, t5
-#   csrw mscratch, t5
-
-#   # Now invoke our M-mode trap handler
-#   csrr a0, mepc
-#   csrr a1, mtval
-#   csrr a2, mcause
-#   csrr a3, mhartid
-#   csrr a4, mstatus
-#   mv a5, t5 # t5 still contains copy of mscratch
-#   # Make sure we use the kernel stack as our trap stack
-#   # instead of that of our user process
-#   # Best practice is probably to allocate a page for
-#   # dedicated use as the trap stack, but using the kernel
-#   # stack directly seems to work for now
-#   la sp, __kernel_stack_end
-#   call m_mode_trap_handler
-
-#   # m_mode_trap_handler returns the PC value via a0
-#   csrw mepc, a0
-
-#   # Restore registers and return
-#   # This is more straightforward, since we can overwrite t6=x31 at the end
-#   csrr t6, mscratch
-#   .set i, 1
-#   .rept 31
-#     load_gp %i
-#     .set i, i + 1
-#   .endr
-
-#   # Continue execution at the given PC value
-#   mret
-
-# .global make_syscall
-# make_syscall:
-#   ecall
-#   ret
-
-# .global switch_to_user
-# switch_to_user:
-#   # a0 - frame address
-#   csrw mscratch, a0
-  
-#   # Set MPP=0 (U-mode), MPIE=1, SPIE=1
-#   li t0, (0b00 << 11) | (1 << 7) | (1 << 5)
-#   csrw mstatus, t0
-
-#   # a1 - program counter
-#   csrw mepc, a1
-
-#   # a2 - SATP register
-#   csrw satp, a2
-  
-#   # Enable external, timer and software interrupts from
-#   # M-mode and S-mode alike
-#   li t0, 0xAAA
-#   csrw mie, t0
-
-#   # Set interrupt handler
-#   la t0, interrupt_handler
-#   csrw mtvec, t0
-
-#   # Sync SATP for all address spaces, for all harts
-#   sfence.vma
-
-#   # Define PMP region to allow (indirect) access to all
-#   # physical memory in U-mode
-#   # By default, M-mode can access all physical memory and
-#   # no other modes can access any physical memory
-#   # pmp0cfg = pmpcfg0[7:0]
-#   #      A=TOR         X=1        W=1        R=1
-#   li t0, (0b01 << 3) | (1 << 2) | (1 << 1) | (1 << 0)
-#   csrw pmpcfg0, t0
-#   # Set all 1's for the top address (exclusive)
-#   # The bottom address (inclusive) is implicitly 0 when setting
-#   # pmpcfg0 and pmpaddr0
-#   li t0, -1
-#   csrw pmpaddr0, t0
-
-#   # Load process context frame
-#   mv t6, a0
-#   .set i, 1
-#   .rept 31
-#     load_gp %i, t6
-#     .set i, i + 1
-#   .endr
-
-#   # Now jump to U-mode
-#   mret
+ # Interrupt handler
+ interrupt_handler:
+   # Save all general purpose registers into kernel trap frame
+   # No need to save floating point registers since we haven't used them yet
+   # No need to save satp, trap_stack since we don't modify them
+   # No need to save hartid since that is always 0
+   # (we only have a single CPU core)
+   # This requires a bit of trickery to do correctly:
+   # 
+   # 0. mscratch has address of kernel trap frame - see kinit() for details
+   # 1. Atomically swap mscratch and t6 registers
+   #    Now t6 has address of kernel trap frame, and mscratch the
+   #    original value of t6
+   # 2. Now save registers x1-x30 into kernel trap frame, using
+   #    t6=x31 as base
+   #    No need to save zero=x0 since that is read-only zero
+   # 3. Move address of kernel trap frame to t5=x30 so we don't lose it
+   # 4. Move mscratch (= original value of t6) back into t6 and save that
+   # 5. Write address of kernel trap frame from t5 back into mscratch
+   csrrw t6, mscratch, t6
+   .set i, 1
+   .rept 30
+     save_gp %i
+     .set i, i + 1
+   .endr
+   mv t5, t6
+   csrr t6, mscratch
+   save_gp 31, t5
+   csrw mscratch, t5
+   # Now invoke our M-mode trap handler
+   csrr a0, mepc
+   csrr a1, mtval
+   csrr a2, mcause
+   csrr a3, mhartid
+   csrr a4, mstatus
+   mv a5, t5 # t5 still contains copy of mscratch
+   # Make sure we use the kernel stack as our trap stack
+   # instead of that of our user process
+   # Best practice is probably to allocate a page for
+   # dedicated use as the trap stack, but using the kernel
+   # stack directly seems to work for now
+   la sp, __kernel_stack_end
+   call m_mode_trap_handler
+   # m_mode_trap_handler returns the PC value via a0
+   csrw mepc, a0
+   # Restore registers and return
+   # This is more straightforward, since we can overwrite t6=x31 at the end
+   csrr t6, mscratch
+   .set i, 1
+   .rept 31
+     load_gp %i
+     .set i, i + 1
+   .endr
+   # Continue execution at the given PC value
+   mret
+ .global make_syscall
+ make_syscall:
+   ecall
+   ret
+ .global switch_to_user
+ switch_to_user:
+   # a0 - frame address
+   csrw mscratch, a0
+ 
+   # Set MPP=0 (U-mode), MPIE=1, SPIE=1
+   li t0, (0b00 << 11) | (1 << 7) | (1 << 5)
+   csrw mstatus, t0
+   # a1 - program counter
+   csrw mepc, a1
+   # a2 - SATP register
+   csrw satp, a2
+ 
+   # Enable external, timer and software interrupts from
+   # M-mode and S-mode alike
+   li t0, 0xAAA
+   csrw mie, t0
+   # Set interrupt handler
+   la t0, interrupt_handler
+   csrw mtvec, t0
+   # Sync SATP for all address spaces, for all harts
+   sfence.vma
+   # Define PMP region to allow (indirect) access to all
+   # physical memory in U-mode
+   # By default, M-mode can access all physical memory and
+   # no other modes can access any physical memory
+   # pmp0cfg = pmpcfg0[7:0]
+   #      A=TOR         X=1        W=1        R=1
+   li t0, (0b01 << 3) | (1 << 2) | (1 << 1) | (1 << 0)
+   csrw pmpcfg0, t0
+   # Set all 1's for the top address (exclusive)
+   # The bottom address (inclusive) is implicitly 0 when setting
+   # pmpcfg0 and pmpaddr0
+   li t0, -1
+   csrw pmpaddr0, t0
+   # Load process context frame
+   mv t6, a0
+   .set i, 1
+   .rept 31
+     load_gp %i, t6
+     .set i, i + 1
+   .endr
+   # Now jump to U-mode
+   mret
--- kernel_riscv/src/kernel.c
+++ kernel_riscv/src/kernel.c
@@ -1,6 +1,12 @@
 #include "drivers/uart/uart.h"
+#include "bootservices/bootservices.h"
+
+void m_mode_trap_handler() {
+  kprintf("Machine mode trap handler\n");
+}
 
 void kmain(void) {
+  get_bootloader_name();
   uart_init();
   kprintf("Initializing the process scheduler ...\n");
 }
